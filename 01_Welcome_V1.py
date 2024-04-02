032af710e0c1bc13986ced81c606fd90271dad79
MOLLY-S1 <98923584+MOLLY-S1@users.noreply.github.com>
2024-04-03 10:11:12 +1300

    Update

---
+++ 01_Welcome_V1.py
@@ -0,0 +1,7 @@
+"""Version !
+This code will create a welcome screen without using TKinter"""
+
+print("Welcome to the Comic Book Store")
+
+user_choice = input("Please enter an option:"
+                    "1) ")
